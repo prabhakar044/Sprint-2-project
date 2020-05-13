e3a16e421f398f8492dcbdafcc0b8d9cf2168f8a
prabhakar044 <59759745+prabhakar044@users.noreply.github.com>
2020-05-13 10:34:21 +0530

    first java commit

---
+++ PaymentWallet/src/test/java/com/example/PaymentWallet/PaymentWalletApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.PaymentWallet;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymentWalletApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
